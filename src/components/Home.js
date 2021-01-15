482a91c47912663138dab5598b4653ed1d400ecd
nicolanzi1 <nicolanzi@gmail.com>
2021-01-15 18:54:39 +0000

    Wrote and styled the Home page using tailwindcss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import image from '../lakesunset.jpg'
 
 const Home = () => {
     return (
-        <h1>Home Page!</h1>
+        <main>
+            <img src={image} alt="Lake Sunset" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hola. I'm Nicola.</h1>
+            </section>
+        </main>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons'
 
 const Navbar = () => {
   return (
-      <h1>Navbar Page!</h1>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink to="/" 
+            exact
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Nicolino
+          </NavLink>
+          <NavLink to="/about"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gold-200 hover:text-green-800"
+          >
+            About Me!
+          </NavLink>
+          <NavLink to="/post"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gold-200 hover:text-green-800"
+          >
+            Blog Post
+          </NavLink>
+          <NavLink to="/project"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gold-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon url="https://www.linkedin.com/in/nicola-lanzillotta-94519042/?locale=en_US" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+          <SocialIcon url="https://github.com/nicolanzi1" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+          <SocialIcon url="https://twitter.com/nicolanzi?lang=es" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+        </div>
+      </div>
+    </header>
   )
 }
 
